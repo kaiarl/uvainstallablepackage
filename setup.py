5821939894de27d9b50e6c110587fbcc2898deb6
Kaia Lindberg <88904949+kaiarl@users.noreply.github.com>
2023-02-05 17:03:17 -0500

    Update version to 1.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='shared',
-      version='1.1.2',
+      version='1.1.3',
       description='This package has shared components.',
       author='Efrain Olivares',
       author_email='efrain.olivares@gmail.com',
