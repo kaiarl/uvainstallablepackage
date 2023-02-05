adcda202303c4229efdb3f49417c7109950e1a04
Kaia Lindberg <88904949+kaiarl@users.noreply.github.com>
2023-02-05 17:11:31 -0500

    Update version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='shared',
-      version='1.1.3',
+      version='1.1.4',
       description='This package has shared components.',
       author='Efrain Olivares',
       author_email='efrain.olivares@gmail.com',
